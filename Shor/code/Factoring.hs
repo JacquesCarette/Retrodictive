63f005066a9b6d8d297c57c72677096e7345d801
Amr Sabry <sabry@indiana.edu>
2021-10-05 20:35:37 -0400

    Factoring.hs

---
+++ Shor/code/Factoring.hs
@@ -0,0 +1,52 @@
+{-# OPTIONS_GHC -XGADTs -XTypeOperators #-}
+
+module Shor where
+
+-----------------------------------------------------------------------------
+-- Goal is to define reversible version of a^x mod N
+-- Devise a reversible language for just this task
+
+data a :<=> b where 
+  Id    :: a :<=> a
+  (:.:) :: (a :<=> b) -> (b :<=> c) -> (a :<=> c)
+  (:*:) :: (a :<=> b) -> (c :<=> d) -> ((a,c) :<=> (b,d))
+  CommuteTimes :: (a,b) :<=> (b,a) 
+  AssocTimesL  :: (a,(b,c)) :<=> ((a,b),c)
+  AssocTimesR  :: ((a,b),c) :<=> (a,(b,c))
+  Even :: (Integer,Bool) :<=> (Integer,Bool)
+  Div2L :: (Integer,Integer) :<=> (Integer,Integer)
+  Div2R :: (Integer,Integer) :<=> (Integer,Integer)
+  MultModL :: (Integer,Integer,Integer,Integer) :<=> (Integer,Integer,Integer,Integer) 
+  MultModR :: (Integer,Integer,Integer,Integer) :<=> (Integer,Integer,Integer,Integer) 
+
+interp :: (a :<=> b) -> a -> b
+interp Id v = v
+interp (c1 :.: c2) v = interp c2 (interp c1 v)
+interp (c1 :*: c2) (v1 , v2) = (interp c1 v1 , interp c2 v2)
+interp CommuteTimes (v1 , v2) = (v2 , v1)
+interp AssocTimesL (v1 , (v2 , v3)) = ((v1 , v2) , v3)
+interp AssocTimesR ((v1 , v2) , v3) = (v1 , (v2 , v3))
+interp Even (n , b) = (n , b /= (n `mod` 2 == 0))
+interp Div2L (n , h) = (n , h + (n `div` 2))
+interp Div2R (n , h) = (n , h - (n `div` 2))
+interp MultModL (a, b, m, h) = (a, b, m, h + ((a * b) `mod` m))
+interp MultModR (a, b, m, h) = (a, b, m, h - ((a * b) `mod` m))
+
+-----------------------------------------------------------------------------
+
+{--
+
+https://byorgey.wordpress.com/2020/02/15/
+	competitive-programming-in-haskell-modular-arithmetic-part-1/
+
+modexp :: Integer -> Integer -> Integer -> Integer
+modexp _ 0 _ = 1
+modexp b e m
+  | even e    = (r*r) `mod` m
+  | otherwise = (b*r*r) `mod` m
+  where
+    r = modexp b (e `div` 2) m
+
+--}
+
+-----------------------------------------------------------------------------
