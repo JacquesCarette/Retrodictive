9135e6602db92af6c95d25b7fb1a4711f0cccfc4
Amr Sabry <sabry@indiana.edu>
2021-10-09 07:40:01 -0400

    good progress addmod and timesmod seem to be working fine

--- Shor/code/ShorPE.hs
+++ Shor/code/ShorPE.hs
@@ -17,19 +17,23 @@ data Op = X Int
         | CX Int Int
         | CCX Int Int Int
         | COp Int Op
+        | SWAP Int Int
         | (:.:) Op Op
         | Alloc Int
         | DeAlloc Int
+        | Loop [Int] (Int -> Op)
 
 invert :: Op -> Op
 invert (X i) = X i
 invert (CX i j) = CX i j
 invert (CCX i j k) = CCX i j k
-invert (op1 :.: op2) = invert op2 :.: invert op1
 invert (COp i op) = COp i (invert op)
+invert (SWAP i j) = SWAP i j
+invert (op1 :.: op2) = invert op2 :.: invert op1
 invert (Alloc i) = DeAlloc i
 invert (DeAlloc i) = Alloc i
-
+invert (Loop indices f) = Loop (reverse indices) (\k -> invert (f k))
+       
 neg :: Vector Bool -> Int -> Vector Bool
 neg vec i = vec // [(i , not (vec ! i))]
 
@@ -48,13 +52,18 @@ interp (COp i op) w@(W n vec)
   | vec ! i = -- trace ("COp: " ++ show w ++ "\n") $
     interp op w
   | otherwise = w
+interp (SWAP i j) w@(W n vec) =
+  W n (vec // [ (i , vec ! j), (j , (vec ! i)) ])
 interp (op1 :.: op2) w = -- trace ("Seq: " ++ show w ++ "\n") $
   interp op2 (interp op1 w)
 interp (Alloc i) w@(W n vec) = -- trace ("Alloc: " ++ show w ++ "\n") $
   W (n+i) (V.replicate i False V.++ vec)
 interp (DeAlloc i) w@(W n vec) = -- trace ("DeAlloc: " ++ show w ++ "\n") $
-  assert (V.all not (V.take i vec)) $   
   W (n-i) (V.drop i vec)
+interp (Loop indices f) w =
+  loop indices w
+  where loop [] w = w
+        loop (i:is) w = loop is (interp (f i) w)
 
 ------------------------------------------------------------------------------
 -- Follow Rieffel & Polak
@@ -103,6 +112,32 @@ addModOp n (ai,ae) (bi,be) (mi,me) =
   DeAlloc 1 :.:
   DeAlloc n
   
+-- shift
+shiftOp :: (Int,Int) -> Op
+shiftOp (i,e) =
+  Loop [i..(e-1)] (\k -> SWAP k (k+1))
+
+-- timesMod a b m p
+timesModOp :: Int -> (Int,Int) -> (Int,Int) -> (Int,Int) -> (Int,Int) -> Op
+timesModOp n (ai,ae) (bi,be) (mi,me) (pi,pe) =
+  Alloc n :.: -- carry
+  Alloc n :.: -- t
+  Loop [0..(n-1)] (\i ->
+    invert (addOp (n,2*n-1) (mi+2*n,me+2*n) (ai+2*n,ae+2*n)) :.:
+    CX (ai+2*n) i :.:
+    COp i (addOp (n,2*n-1) (mi+2*n,me+2*n) (ai+2*n,ae+2*n)) :.: 
+    COp (be+2*n-i) (addModOp n (ai+2*n+1,ae+2*n) (pi+2*n,pe+2*n) (mi+2*n,me+2*n)) :.: 
+    shiftOp (ai+2*n,ae+2*n) 
+  ) :.:
+  Loop [(n-1),(n-2)..0] (\i ->
+    invert (shiftOp (ai+2*n,ae+2*n)) :.:
+    COp i (invert (addOp (n,2*n-1) (mi+2*n,me+2*n) (ai+2*n,ae+2*n))) :.: 
+    CX (ai+2*n) i :.:
+    addOp (n,2*n-1) (mi+2*n,me+2*n) (ai+2*n,ae+2*n)
+  ) :.:
+  DeAlloc n :.:
+  DeAlloc n
+
 ------------------------------------------------------------------------------
 -- Testing
 
@@ -129,16 +164,25 @@ addGen = do n <- chooseInt (1, 20)
             return (W wn (fromList (cs ++ as ++ bs)))
 
 addModGen :: Gen W
-addModGen = do n <- chooseInt (2, 10)
+addModGen = do n <- chooseInt (2, 20)
                let wn = 3 * n + 1
                as <- vector n
                lowbsms <- suchThat (vector (2*n)) $ \bits ->
                             bools2nat (drop n bits) > max 1 (bools2nat (take n bits))
                return (W wn (fromList (as ++ (False : lowbsms))))
 
+timesModGen :: Gen W
+timesModGen = do n <- chooseInt (2, 2)
+                 let wn = 4 * n + 2
+                 lowasbsms <- suchThat (vector (3*n)) $ \bits ->
+                                bools2nat (take n bits) < bools2nat (drop (2*n) bits)
+                 let ps = replicate n False 
+                 return (W wn (fromList ((False : lowasbsms) ++ (False : ps))))
+
 instance Arbitrary W where
 --  arbitrary = addGen
-  arbitrary = addModGen
+--  arbitrary = addModGen
+    arbitrary = timesModGen
 
 addProp :: W -> Bool
 addProp w@(W wn vec) =
@@ -166,5 +210,19 @@ addModProp w@(W wn vec) =
       expected = W wn (fromList (as ++ nat2bools (n+1) sum ++ ms))
   in actual == expected 
 
+timesModProp :: W -> Bool
+timesModProp w@(W wn vec) =
+  let n = (wn - 2) `div` 4
+      actual = interp (timesModOp n (0,n) (n+1,2*n) (2*n+1,3*n) (3*n+1,4*n+1)) w
+      (as,r) = splitAt (n+1) (toList vec)
+      (bs,r') = splitAt n r
+      (ms,ps) = splitAt n r'
+      a = bools2nat as
+      b = bools2nat bs
+      m = bools2nat ms
+      p = bools2nat ps
+      res = (p + b * a) `mod` m
+      expected = W wn (fromList (as ++ bs ++ ms ++ nat2bools (n+1) res))
+  in actual == expected 
 
 ------------------------------------------------------------------------------
