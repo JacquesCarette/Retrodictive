ae3d55b9a2557c61972d4570b1fcb60dc192ad5a
Amr Sabry <sabry@indiana.edu>
2021-10-12 11:53:42 -0400

    a more PL perspective on circuits in ShorCircuit2

--- Shor/code/ShorCircuit2.hs
+++ Shor/code/ShorCircuit2.hs
@@ -1,5 +1,9 @@
 {-# LANGUAGE TemplateHaskell #-}
 
+-- Ref:
+-- Quantum Networks for Elementary Arithmetic Operations
+-- Vlatko Vedral∗, Adriano Barenco and Artur Ekert
+
 module Shor where
 
 import Data.Char
@@ -22,6 +26,8 @@ trace :: String -> a -> a
 trace s a = if debug then Debug.Trace.trace s a else a
 
 ------------------------------------------------------------------------------
+-- Abstract operations to be given either a conventional semantics or
+-- a symbolic one
 
 type Var = Int
 
@@ -31,14 +37,12 @@ class Monad m => Circuit m where
   cx   :: (Bit,Bit) -> m (Bit,Bit)
   ccx  :: (Bit,Bit,Bit) -> m (Bit,Bit,Bit)
 
--- sum: c, a, b => c, a, (a+b+c) mod 2
+-- Is there an elegant way to invert circuits???
 
-sumOP :: Circuit m => (Bit,Bit,Bit) -> m (Bit,Bit,Bit)
-sumOP (c,a,b) =
-  do (a,b) <- cx (a,b)
-     (c,b) <- cx (c,b)
-     return (c,a,b)
+invert :: Circuit m => (a -> m b) -> b -> m a
+invert f out = undefined
 
+------------------------------------------------------------------------------
 -- Standard interpreter
 
 data Std a = Std a
@@ -66,10 +70,31 @@ instance Circuit Std where
     | ctrl1 && ctrl2 = return (Bool ctrl1, Bool ctrl2, Bool (not target))
     | otherwise = return arg
 
+------------------------------------------------------------------------------
+-- Partial evaluator
+
+------------------------------------------------------------------------------
+-- Actual circuits
+
+sumOP :: Circuit m => (Bit,Bit,Bit) -> m (Bit,Bit,Bit)
+sumOP (c,a,b) =
+  do (a,b) <- cx (a,b)
+     (c,b) <- cx (c,b)
+     return (c,a,b)
+
+carryOP :: Circuit m => (Bit,Bit,Bit,Bit) -> m (Bit,Bit,Bit,Bit)
+carryOP (c,a,b,c') =
+  do (a,b,c') <- ccx (a,b,c')
+     (a,b) <- cx (a,b)
+     (c,b,c') <- ccx (c,b,c')
+     return (c,a,b,c')
+
+
+------------------------------------------------------------------------------
+-- Testing...
 
 test :: (Bit,Bit,Bit)
 test = simulate $ sumOP (Bool True, Bool False, Bool False)
-
 
 ------------------------------------------------------------------------------
 ------------------------------------------------------------------------------
