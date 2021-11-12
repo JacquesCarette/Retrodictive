0a14fbb6e22395d7901413cb31268bd651d10878
Amr Sabry <sabry@indiana.edu>
2021-11-11 20:59:53 -0500

    added longer list of primes for testing

--- Shor/code/ShorOptimizedFourierBasis.hs
+++ Shor/code/ShorOptimizedFourierBasis.hs
@@ -559,17 +559,18 @@ runPE n a m res = pretty $ runST $ do
 -- it seems that the period is between 2^(v-1) and 2(v+1) for
 -- the v we chose (y = 2^v)
 
-searchAround :: Integer -> Integer -> Integer -> Maybe Integer
-searchAround  y m a =
-  if powModInteger a y m == 1 then Just y
-  else 
-    let s = maybe
-            (searchAround (y-2) m a)
-            Just
-            (searchAround (y+2) m a)
-    in if s == Nothing
-        then searchAround y m a
-        else s
+searchAround :: Int -> Integer -> Integer -> Integer -> Maybe Integer
+searchAround  n y m a = loop (n * n * n) y -- max about (log N)^3
+  where loop 0 y = Nothing
+        loop i y = 
+          if powModInteger a y m == 1 then Just y
+          else let s = maybe
+                       (loop (i-1) (y-2))
+                       Just
+                       (loop (i-1) (y+2))
+               in if s == Nothing
+                  then loop (i-1) y
+                  else s
 
 factor :: Integer -> IO (Integer,Integer)
 factor m = do
@@ -591,14 +592,15 @@ factor m = do
 --      else do
     let y = 2 ^ numberVars
     putStrLn (printf "Searching for period around %d" y)
-    case searchAround y m a of
+    case searchAround n y m a of
       Nothing -> factor m
       Just s ->
         D.trace (printf "Found period %d (after testing %d of %d (or %.6f %%)"
-                  s (abs (s-y)) q
+                  s (abs (s-y)) (n * n * n)
                   (let sd :: Double = fromInteger s
                        yd :: Double = fromInteger y
-                   in (100.0 * (abs (sd - yd)) / (2 ^ (n+1))))) $ 
+                       nd :: Double = fromInteger (toInteger n)
+                   in (100.0 * (abs (sd - yd)) / (nd * nd * nd)))) $ 
         let (f1,f2) = (gcd (powModInteger a (s `div` 2) m - 1) m,
                        gcd (powModInteger a (s `div` 2) m + 1) m)
         in if f1 == 1 || f2 == 1
@@ -606,11 +608,29 @@ factor m = do
            else return (f1,f2)
 
 primes :: [Integer]
-primes = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,
-          61,67,71,73,79,83,89,97,101,103,107,109,113,127,
-          131,137,139,149,151,157,163,167,173,179,181,191,
-          193,197,199,211,223,227,229,233,239,241,251,257,
-          263,269,271]
+primes = [
+  2,    3,      5,      7,      11,     13,     17,     19,     23,     29,
+  31,   37,     41,     43,     47,     53,     59,     61,     67,     71,
+  73,   79,     83,     89,     97,     101,    103,    107,    109,    113,
+  127,  131,    137,    139,    149,    151,    157,    163,    167,    173,
+  179,  181,    191,    193,    197,    199,    211,    223,    227,    229,
+  233,  239,    241,    251,    257,    263,    269,    271,    277,    281,
+  283,  293,    307,    311,    313,    317,    331,    337,    347,    349,
+  353,  359,    367,    373,    379,    383,    389,    397,    401,    409,
+  419,  421,    431,    433,    439,    443,    449,    457,    461,    463,
+  467,  479,    487,    491,    499,    503,    509,    521,    523,    541,
+  547,  557,    563,    569,    571,    577,    587,    593,    599,    601,
+  607,  613,    617,    619,    631,    641,    643,    647,    653,    659,
+  661,  673,    677,    683,    691,    701,    709,    719,    727,    733,
+  739,  743,    751,    757,    761,    769,    773,    787,    797,    809,
+  811,  821,    823,    827,    829,    839,    853,    857,    859,    863,
+  877,  881,    883,    887,    907,    911,    919,    929,    937,    941,
+  947,  953,    967,    971,    977,    983,    991,    997,    1009,   1013,
+  1019, 1021,   1031,   1033,   1039,   1049,   1051,   1061,   1063,   1069,
+  1087, 1091,   1093,   1097,   1103,   1109,   1117,   1123,   1129,   1151,
+  1153, 1163,   1171,   1181,   1187,   1193,   1201,   1213,   1217,   1223
+  ]
+
 
 test :: IO Integer
 test= do
