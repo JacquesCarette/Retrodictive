a7fc5000d6afa9c49081d9147acc4ed1fa18f60a
Jacques Carette <carette@mcmaster.ca>
2022-02-01 21:44:28 -0500

    defensive programming. Haskell is so sorely missing non-negative types...

--- execution/RetroPE/src/Value.hs
+++ execution/RetroPE/src/Value.hs
@@ -1,8 +1,8 @@
 module Value where
 
 import Prelude hiding (seq)
-import Control.Monad.ST
-import Data.STRef
+import Control.Monad.ST (ST)
+import Data.STRef (STRef, newSTRef)
 
 ----------------------------------------------------------------------------------------
 -- Circuits manipulate locations holding values
@@ -15,12 +15,19 @@ class (Show v, Enum v) => Value v where
   sxor :: v -> v -> v
 
 bin :: Value v => Integer -> [v]
+bin n | n < 0 = error "Panic: (bin) Integer is negative!"
 bin 0 = []
 bin n = let (q,r) = quotRem n 2 in toEnum (fromInteger r) : bin q
 
+-- fromInt takes an Integer and pads it out (on the right) with zeros so that
+-- the result is |len| long.
 fromInt :: Value v => Int -> Integer -> [v]
-fromInt len n = bits ++ replicate (len - length bits) zero
+fromInt len n = 
+  if len < 0 then error "Panic: (fromInt) trying to truncate?"
+  else if l <= 0 then error "Panic: (fromInt) asking for negative number of bits"
+  else bits ++ replicate (len - length bits) zero
   where bits = bin n
+        l = len - length bits
 
 type Var s v = STRef s v
 
