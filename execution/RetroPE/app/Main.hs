5f565ca34802ab483d558040a0db40103758d79d
Jacques Carette <carette@mcmaster.ca>
2022-03-02 13:34:45 -0500

    test on Shor instead.

--- execution/RetroPE/app/Main.hs
+++ execution/RetroPE/app/Main.hs
@@ -1,7 +1,8 @@
 module Main where
 
-import PEZ (retroGrover)
+import PEZ (retroGrover, retroShor)
 
 main :: IO ()
 main = do
-  mapM_ (retroGrover 5) [0..31]
+  retroShor 51 -- should be fast: 15, 21, 51, 83, 771; slower: 21, 35
+  -- mapM_ (retroGrover 5) [0..31]
