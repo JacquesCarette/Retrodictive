1665f17bfb75fb9bdda2873ccec84695130fc3fd
Jacques Carette <carette@mcmaster.ca>
2022-10-14 13:55:19 +0100

    writing up a description of the code made it clear the ZValue instance was a redundant wrapping of Bool, so eliminate it.

--- execution/RetroPE/src/EvalZ.hs
+++ execution/RetroPE/src/EvalZ.hs
@@ -12,43 +12,24 @@ import Circuits (OP, Circuit(op))
 
 ------------------------------------------------------------------------------
 -- Evaluate a circuit in the Z basis (the computational basis)
-
--- Values
-
-newtype ZValue = ZValue Bool
-
-instance Enum ZValue where
-  toEnum 0 = ZValue False
-  toEnum 1 = ZValue True
-  fromEnum (ZValue False) = 0
-  fromEnum (ZValue True) = 1
-
-instance Show ZValue where
-  show (ZValue b) = show b
-
-instance Value ZValue where
-  zero = ZValue False
-  one = ZValue True
-  snot (ZValue b) = ZValue (not b)
-  sand (ZValue b1) (ZValue b2) = ZValue (b1 && b2)
-  sxor (ZValue b1) (ZValue b2) = ZValue (b1 /= b2)
+-- Use Bool directly as the type.
 
 ------------------------------------------------------------------------------
 -- Interpreter
 
-controlsActive :: [Bool] -> [ZValue] -> Bool
-controlsActive bs cs = and (zipWith (\ b (ZValue b') -> b == b') bs cs)
+controlsActive :: [Bool] -> [Bool] -> Bool
+controlsActive bs cs = and (zipWith (==) bs cs)
 
-interpGT :: GToffoli (Var s ZValue) -> ST s ()
+interpGT :: GToffoli (Var s Bool) -> ST s ()
 interpGT (GToffoli bs cs t) = do
   controls <- mapM readSTRef cs
   tv <- readSTRef t
   when (controlsActive bs controls) $ writeSTRef t (snot tv)
 
-interp :: OP (Var s ZValue) -> ST s ()
+interp :: OP (Var s Bool) -> ST s ()
 interp = foldMap interpGT
 
-run :: Circuit s ZValue -> ST s ()
+run :: Circuit s Bool -> ST s ()
 run = interp . op
  
 ------------------------------------------------------------------------------
