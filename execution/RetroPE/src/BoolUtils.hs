51e801200dc73178e17da90b8650b3a86db48227
carette@mcmaster.ca <carette@mcmaster.ca>
2023-01-03 13:22:43 -0500

    the code is now in a different repo

--- execution/RetroPE/src/BoolUtils.hs
+++
@@ -1,9 +0,0 @@
-module BoolUtils where
-  
-------------------------------------------------------------------------------
-
--- Interpret a list of booleans as an Integer
-toInt :: [Bool] -> Integer
-toInt = foldr (\ b n -> toInteger (fromEnum b) + 2*n) 0
-
-------------------------------------------------------------------------------
